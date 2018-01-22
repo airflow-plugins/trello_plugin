f36f6daf2511c9e5a34a5af4592c2909a7580dc8
Adelina Enache <enache.adelina99@gmail.com>
2018-01-22 16:52:13 +0200

    Add SkipMixin

--- operators/trello_to_s3_operator.py
+++ operators/trello_to_s3_operator.py
@@ -1,10 +1,12 @@
-from airflow.hooks.S3_hook import S3Hook
-from airflow.models import BaseOperator
-
-from trello_plugin.hooks.trello_hook import TrelloHook
+import json
+import logging
 
 from tempfile import NamedTemporaryFile
-import json
+
+from airflow.hooks.S3_hook import S3Hook
+from airflow.models import BaseOperator, SkipMixin
+
+from trello_plugin.hooks.trello_hook import TrelloHook
 
 
 class EndpointNotSupported(Exception):
@@ -13,7 +15,7 @@ class EndpointNotSupported(Exception):
         super().__init__("Specified endpoint not currently supported.")
 
 
-class TrelloToS3Operator(BaseOperator):
+class TrelloToS3Operator(BaseOperator, SkipMixin):
     """
     Trello to S3 Operator
     :param trello_conn_id:          The Airflow id used to store the Trello
@@ -97,7 +99,7 @@ class TrelloToS3Operator(BaseOperator):
 
         for endpoint in endpoints:
             result = self.hook.run(
-                endpoint, fields=self.fields, extra_args=extra_args).json()
+                endpoint, extra_args=extra_args).json()
             results += result
 
         return results
@@ -136,19 +138,35 @@ class TrelloToS3Operator(BaseOperator):
                                       extra_args=extra_args) for board in boards]
             results = self.get_all(endpoints, extra_args=extra_args)
 
-        with NamedTemporaryFile("w") as tmp:
-            for result in results:
-                tmp.write(json.dumps(result) + '\n')
+        if len(results) == 0 or results is None:
+            logging.info("No records pulled from Trello.")
+            downstream_tasks = context['task'].get_flat_relatives(
+                upstream=False)
+            logging.info('Skipping downstream tasks...')
+            logging.debug("Downstream task_ids %s", downstream_tasks)
 
-            tmp.flush()
+            if downstream_tasks:
+                self.skip(context['dag_run'],
+                          context['ti'].execution_date,
+                          downstream_tasks)
+            return True
 
-            dest_s3 = S3Hook(s3_conn_id=self.s3_conn_id)
-            dest_s3.load_file(
-                filename=tmp.name,
-                key=self.s3_key,
-                bucket_name=self.s3_bucket,
-                replace=True
+        else:
+            # Write the results to a temporary file and save that file to s3.
+            with NamedTemporaryFile("w") as tmp:
+                for result in results:
+                    filtered_result = self.filter_fields(result)
+                    tmp.write(json.dumps(filtered_result) + '\n')
 
-            )
-            dest_s3.connection.close()
-            tmp.close()
+                tmp.flush()
+
+                dest_s3 = S3Hook(s3_conn_id=self.s3_conn_id)
+                dest_s3.load_file(
+                    filename=tmp.name,
+                    key=self.s3_key,
+                    bucket_name=self.s3_bucket,
+                    replace=True
+
+                )
+                dest_s3.connection.close()
+                tmp.close()
